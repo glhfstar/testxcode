8cc53cbd189ec770269fcf6e860a5d692c9fbe1c
qihua <qihua@xieruihuadeMac-mini.local>
2017-11-27 15:34:43 +0800

    add project

---
+++ testxcode/testxcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testxcode
+//
+//  Created by qihua  on 2017/11/27.
+//  Copyright © 2017年 qihua . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
